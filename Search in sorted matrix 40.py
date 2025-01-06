625c935f3e1a03da84bb506f01c7149492101cc1
Deeksha-coder-debug <deeksha.jeypore@gmail.com>
2025-01-06 09:51:25 +0530

    Create Search in sorted matrix 40.py
    Problem statement : 
    
    Search in a sorted Matrix
    
    Difficulty: MediumAccuracy: 56.27%Submissions: 125K+Points: 4
    
    Given a strictly sorted 2D matrix mat[][] of size n x m and a number x. Find whether the number x is present in the matrix or not.
    Note: In a strictly sorted matrix, each row is sorted in strictly increasing order, and the first element of the ith row (i!=0) is greater than the last element of the (i-1)th row.
    
    Examples:
    
    Input: mat[][] = [[1, 5, 9], [14, 20, 21], [30, 34, 43]], x = 14
    Output: true
    Explanation: 14 is present in the matrix, so output is true.
    Input: mat[][] = [[1, 5, 9, 11], [14, 20, 21, 26], [30, 34, 43, 50]], x = 42
    Output: false
    Explanation: 42 is not present in the matrix.

---
+++ Search in sorted matrix 40.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    #Function to search a given number in row-column sorted matrix.
+    def searchMatrix(self, mat, x): 
+        # code here 
+        for row in mat:
+            st=0
+            end=len(mat[0])-1
+            while st<=end:
+                mid=st+(end-st)//2
+                if row[mid]==x:
+                    return True
+                elif row[mid]>x:
+                    end=mid-1
+                else:
+                    st=mid+1
+        return False
+        
